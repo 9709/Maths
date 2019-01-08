21cd536da4c98ecb69a32f6e41d5dcc4c14a7ddb
Matthew Chan <mat_t4@hotmail.com>
2019-01-08 14:50:05 -0800

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Matthew Chan on 2019-01-08.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
